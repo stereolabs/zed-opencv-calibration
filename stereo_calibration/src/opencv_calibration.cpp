8dd778a796747c7250973b7a6c7973bd62e581ab
Walter Lucetti <walter.lucetti@stereolabs.com>
2025-11-20 13:11:51 +0100

    Save SDK calib file

--- stereo_calibration/src/opencv_calibration.cpp
+++ stereo_calibration/src/opencv_calibration.cpp
@@ -2,8 +2,8 @@
 
 int calibrate(int img_count, const std::string& folder, StereoCalib& calib_data,
               int target_w, int target_h, float square_size, int serial,
-              bool is_dual_mono, bool is_4k, bool save_calib_mono, bool use_intrinsic_prior,
-              float max_repr_error, bool verbose) {
+              bool is_dual_mono, bool is_4k, bool save_calib_mono,
+              bool use_intrinsic_prior, float max_repr_error, bool verbose) {
   std::vector<cv::Mat> left_images, right_images;
 
   /// Read images
@@ -124,7 +124,8 @@ int calibrate(int img_count, const std::string& folder, StereoCalib& calib_data,
     if (rms_l > 0.5f || rms_r > 0.5f || err > 0.5f) {
       std::cout << std::endl
                 << "\t !! Warning !!" << std::endl
-                << "The max reprojection error looks too high (>" << max_repr_error
+                << "The max reprojection error looks too high (>"
+                << max_repr_error
                 << "), check that the lenses are clean (sharp images)"
                    " and that the pattern is printed/mounted on a RIGID "
                    "and FLAT surface."
@@ -154,19 +155,22 @@ int calibrate(int img_count, const std::string& folder, StereoCalib& calib_data,
 
     std::string opencv_file = calib_data.saveCalibOpenCV(serial);
     if (!opencv_file.empty()) {
-      std::cout << " * OpenCV calibration file saved: " << opencv_file << std::endl;
+      std::cout << " * OpenCV calibration file saved: " << opencv_file
+                << std::endl;
     } else {
-      std::cout << " !!! Failed to save OpenCV calibration file "
-                << opencv_file << " !!!" << std::endl;
+      std::cout << " !!! Failed to save OpenCV calibration file " << opencv_file
+                << " !!!" << std::endl;
     }
 
     // SDK format is only supported for dual-mono setups
     if (is_dual_mono) {
       std::string zed_file = calib_data.saveCalibZED(serial, is_4k);
       if (!zed_file.empty()) {
-        std::cout << " * ZED SDK calibration file saved: " << zed_file << std::endl;
+        std::cout << " * ZED SDK calibration file saved: " << zed_file
+                  << std::endl;
       } else {
-        std::cout << " !!! Failed to save ZED SDK calibration file " << zed_file << " !!!" << std::endl;
+        std::cout << " !!! Failed to save ZED SDK calibration file " << zed_file
+                  << " !!!" << std::endl;
       }
     }
   }
@@ -186,8 +190,7 @@ std::string StereoCalib::saveCalibOpenCV(int serial) {
     if (left.disto_model_RadTan) {
       fs << "D_LEFT" << left.D << "D_RIGHT" << right.D;
     } else {
-      fs << "D_LEFT_FE" << left.D << "D_RIGHT_FE"
-         << right.D;
+      fs << "D_LEFT_FE" << left.D << "D_RIGHT_FE" << right.D;
     }
 
     fs << "R" << Rv << "T" << T;
@@ -200,7 +203,210 @@ std::string StereoCalib::saveCalibOpenCV(int serial) {
 }
 
 std::string StereoCalib::saveCalibZED(int serial, bool is_4k) {
+  std::string calib_filename = "SN" + std::to_string(serial) + ".conf";
 
+  // Write parameters to a text file
+  std::ofstream outfile(calib_filename);
+  if (!outfile.is_open()) {
+    std::cerr
+        << " !!! Cannot save the calibration file: 'Unable to open output file'"
+        << std::endl;
+    return std::string();
+  }
 
-  return std::string(); 
+  if (!is_4k) {  //  AR0234
+    outfile << "[LEFT_CAM_FHD1200]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) << "\n\n";
+
+    outfile << "[RIGHT_CAM_FHD1200]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) << "\n\n";
+
+    outfile << "[LEFT_CAM_FHD]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) - 60 << "\n\n";
+
+    outfile << "[RIGHT_CAM_FHD]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) - 60 << "\n\n";
+
+    outfile << "[LEFT_CAM_SVGA]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) / 2 << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) / 2 << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) / 2 << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) / 2 << "\n\n";
+
+    outfile << "[RIGHT_CAM_SVGA]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) / 2 << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) / 2 << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) / 2 << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) / 2 << "\n\n";
+
+    // Add other parameters for other cameras...
+
+    outfile << "[LEFT_DISTO]\n";
+    outfile << "k1 = " << left.D.at<double>(0) << "\n";
+    outfile << "k2 = " << left.D.at<double>(1) << "\n";
+    outfile << "p1 = " << left.D.at<double>(2) << "\n";
+    outfile << "p2 = " << left.D.at<double>(3) << "\n";
+    outfile << "k3 = " << left.D.at<double>(4) << "\n";
+    outfile << "k4 = " << left.D.at<double>(5) << "\n";
+    outfile << "k5 = " << left.D.at<double>(6) << "\n";
+    outfile << "k6 = " << left.D.at<double>(7) << "\n\n";
+
+    outfile << "[RIGHT_DISTO]\n";
+    outfile << "k1 = " << right.D.at<double>(0) << "\n";
+    outfile << "k2 = " << right.D.at<double>(1) << "\n";
+    outfile << "p1 = " << right.D.at<double>(2) << "\n";
+    outfile << "p2 = " << right.D.at<double>(3) << "\n";
+    outfile << "k3 = " << right.D.at<double>(4) << "\n";
+    outfile << "k4 = " << right.D.at<double>(5) << "\n";
+    outfile << "k5 = " << right.D.at<double>(6) << "\n";
+    outfile << "k6 = " << right.D.at<double>(7) << "\n\n";
+
+    outfile << "[STEREO]\n";
+    outfile << "Baseline = " << -T.at<double>(0) << "\n";
+    outfile << "TY = " << T.at<double>(1) << "\n";
+    outfile << "TZ = " << T.at<double>(2) << "\n";
+    outfile << "CV_FHD = " << R.at<double>(1) << "\n";
+    outfile << "CV_SVGA = " << R.at<double>(1) << "\n";
+    outfile << "CV_FHD1200 = " << R.at<double>(1) << "\n";
+    outfile << "RX_FHD = " << R.at<double>(0) << "\n";
+    outfile << "RX_SVGA = " << R.at<double>(0) << "\n";
+    outfile << "RX_FHD1200 = " << R.at<double>(0) << "\n";
+    outfile << "RZ_FHD = " << R.at<double>(2) << "\n";
+    outfile << "RZ_SVGA = " << R.at<double>(2) << "\n";
+    outfile << "RZ_FHD1200 = " << R.at<double>(2) << "\n\n";
+
+    // Add other parameters for other stereo parameters if needed...
+
+    outfile << "[MISC]\n";
+    outfile << "Sensor_ID = 1\n\n";
+
+    outfile.close();
+    std::cout << " * Parameter file written successfully: '" << calib_filename
+              << "'" << std::endl;
+    return calib_filename;
+  } else {  //  IMX678
+    outfile << "[LEFT_CAM_4k]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) << "\n\n";
+
+    outfile << "[RIGHT_CAM_4k]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) << "\n\n";
+
+    outfile << "[LEFT_CAM_QHDPLUS]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) - (3840 - 3200) / 2
+            << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) - (2160 - 1800) / 2
+            << "\n\n";
+
+    outfile << "[RIGHT_CAM_QHDPLUS]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) - (3840 - 3200) / 2
+            << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) - (2160 - 1800) / 2
+            << "\n\n";
+
+    outfile << "[LEFT_CAM_FHD]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) / 2 << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) / 2 << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) / 2 << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) / 2 << "\n\n";
+
+    outfile << "[RIGHT_CAM_FHD]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) / 2 << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) / 2 << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) / 2 << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) / 2 << "\n\n";
+
+    outfile << "[LEFT_CAM_FHD1200]\n";
+    outfile << "fx = " << left.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << left.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << left.K.at<double>(0, 2) - (3840 - 1920) / 2
+            << "\n";
+    outfile << "cy = " << left.K.at<double>(1, 2) - (2160 - 1200) / 2
+            << "\n\n";
+
+    outfile << "[RIGHT_CAM_FHD1200]\n";
+    outfile << "fx = " << right.K.at<double>(0, 0) << "\n";
+    outfile << "fy = " << right.K.at<double>(1, 1) << "\n";
+    outfile << "cx = " << right.K.at<double>(0, 2) - (3840 - 1920) / 2
+            << "\n";
+    outfile << "cy = " << right.K.at<double>(1, 2) - (2160 - 1200) / 2
+            << "\n\n";
+
+    // Add other parameters for other cameras...
+
+    outfile << "[LEFT_DISTO]\n";
+    outfile << "k1 = " << left.D.at<double>(0) << "\n";
+    outfile << "k2 = " << left.D.at<double>(1) << "\n";
+    outfile << "p1 = " << left.D.at<double>(2) << "\n";
+    outfile << "p2 = " << left.D.at<double>(3) << "\n";
+    outfile << "k3 = " << left.D.at<double>(4) << "\n";
+    outfile << "k4 = " << left.D.at<double>(5) << "\n";
+    outfile << "k5 = " << left.D.at<double>(6) << "\n";
+    outfile << "k6 = " << left.D.at<double>(7) << "\n\n";
+
+    outfile << "[RIGHT_DISTO]\n";
+    outfile << "k1 = " << right.D.at<double>(0) << "\n";
+    outfile << "k2 = " << right.D.at<double>(1) << "\n";
+    outfile << "p1 = " << right.D.at<double>(2) << "\n";
+    outfile << "p2 = " << right.D.at<double>(3) << "\n";
+    outfile << "k3 = " << right.D.at<double>(4) << "\n";
+    outfile << "k4 = " << right.D.at<double>(5) << "\n";
+    outfile << "k5 = " << right.D.at<double>(6) << "\n";
+    outfile << "k6 = " << right.D.at<double>(7) << "\n\n";
+
+    outfile << "[STEREO]\n";
+    outfile << "Baseline = " << -T.at<double>(0) << "\n";
+    outfile << "TY = " << T.at<double>(1) << "\n";
+    outfile << "TZ = " << T.at<double>(2) << "\n";
+    outfile << "CV_FHD = " << R.at<double>(1) << "\n";
+    outfile << "CV_FHD1200 = " << R.at<double>(1) << "\n";
+    outfile << "CV_4k = " << R.at<double>(1) << "\n";
+    outfile << "CV_QHDPLUS = " << R.at<double>(1) << "\n";
+    outfile << "RX_FHD = " << R.at<double>(0) << "\n";
+    outfile << "RX_FHD1200 = " << R.at<double>(0) << "\n";
+    outfile << "RX_4k = " << R.at<double>(0) << "\n";
+    outfile << "RX_QHDPLUS = " << R.at<double>(0) << "\n";
+    outfile << "RZ_FHD = " << R.at<double>(2) << "\n";
+    outfile << "RZ_FHD1200 = " << R.at<double>(2) << "\n";
+    outfile << "RZ_4k = " << R.at<double>(2) << "\n\n";
+    outfile << "RZ_QHDPLUS = " << R.at<double>(2) << "\n\n";
+
+    // Add other parameters for other stereo parameters if needed...
+
+    outfile << "[MISC]\n";
+    outfile << "Sensor_ID = 2\n\n";
+
+    outfile.close();
+    std::cout << " * Parameter file written successfully: '" << calib_filename
+              << "'" << std::endl;
+    return calib_filename;
+  }
+
+  std::cout
+      << "The resolution for the calibration is not valid\n\nUse 4k "
+         "(3840x2160) for ZED X One 4K and FHD1200 (1920x1200) for ZED X One GS"
+      << std::endl;
+
+  return std::string();
 }
